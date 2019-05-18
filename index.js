6314041ea0c37c36e913bc0bbfd21d1c9642cb56
Youssef Meskini <youssef@smartsailors.net>
2019-05-18 19:28:27 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const fathah = "\u064e";
+const dhamma = "\u064f";
+const kasra = "\u0650";
+
+const ya_hamza = "\u0626";
+const joining_hamza = "\u0654";
+
+const kasratain = "\u064d";
+const fathatain = "\u064b";
+const dammatain = "\u064c";
+const shadda = "\u0651";
+const space = " ";
+
+//arabic letters
+const nun = "\u0646";
+const mim = "\u0645";
+const qaf = "\u0642";
+const toa = "\u0637";
+const ba = "\u0628";
+const zim = "\u062c";
+const dal = "\u062f";
+const soad = "\u0635";
+const zaal = "\u0630";
+const tha = "\u062b";
+const kaf = "\u0643";
+const wow = "\u0648";
+const shin = "\u0634";
+const seen = "\u0633";
+const zha = "\u0632";
+const fa = "\u0641";
+const ta = "\u062a";
+const doad = "\u0636";
+const zoa = "\u0638";
+const ra = "\u0631";
+const lam = "\u0644";
+
+//stop signs uthmani
+const stop_sign_zim = "\u06da";
+const stop_sign_lam = "\u06d9";
+const stop_sign_high_seen = "\u06dc";
+const stop_sign_mim = "\u06d8";
+const stop_sign_three_dots = "\u06db";
+const stop_sign_qaf_lam = "\u06d7";
+const stop_sign_soad_lam = "\u06d6";
+
+const low_seen = "\u06e3";
+
+//sukuns
+const sukun = "\u0652";
+const curvy_sukun = "\u06e1";
+const small_rounded_zero = "\u06df";
+
+//others
+const low_meem = "\u06ed";
+const high_meem = "\u06e2";
+const alif_hamza = "\u0627";
+const empty_ya = alif_hamza;
+const empty_alif = "\u0649";
+const another_ya = "\u064a";
+const ta_marbuta = "\u0647";
+const supercript_alif_khara_fatha = "\u0670";
+const small_waw_elongation = "\u06E5";
+const small_ya_elongation = "\u06E6";
+const tatweel_empty_letter = "\u0640";
+const high_small_ya = "\u06E7";
